3c2cd202e1c2a1a8c4052c74085e88c7692cafd4
connorcurtis <connorcurtis@mail.weber.edu>
2022-01-26 17:14:00 -0700

    added profile to user, added date and staus for registration

--- Assignment1/Assignment1/users/admin.py
+++ Assignment1/Assignment1/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import WelcomePage
+from .models import Profile
 
 # Register your models here.
 
 
-admin.site.register(WelcomePage)
+admin.site.register(Profile)
