15c5b2538a644634a51eec3b9acded7483ec8db9
Vipenjit Singh <vipenjitsingh@mail.weber.edu>
2022-01-28 15:04:01 -0700

    Changed Login and register forms

--- Assignment1/Assignment1/users/models.py
+++ Assignment1/Assignment1/users/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField()
-    status = models.CharField(max_length=20)
+
+    ##status = models.CharField(max_length=20, default="active")
 
     def __str__(self):
         return self.user.username
-
--- Assignment1/Assignment1/users/views.py
+++ Assignment1/Assignment1/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegistrationForm
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
 
@@ -9,13 +11,15 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}!')
             return redirect('login')
+
     else:
         form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form})
-
 
 
