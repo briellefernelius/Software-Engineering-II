e7bb9fe64241e181bfd4435d0d34c1114ea2b36e
connorcurtis <connorcurtis@mail.weber.edu>
2022-01-29 01:37:39 -0700

    birthday validation

--- Assignment1/Assignment1/users/forms.py
+++ Assignment1/Assignment1/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from datetime import date
 
 
 options=[('student', 'Student'),
@@ -11,6 +12,13 @@ class RegistrationForm(UserCreationForm):
     birthday = forms.DateField()
 
     Status = forms.CharField(label='Are you a student or instructor?', widget = forms.RadioSelect(choices = options))
+
+    def clean_birthday(self):
+        bday = self.cleaned_data['birthday']
+        age = (date.today() - bday).days/362
+        if age < 16:
+            raise forms.ValidationError('Must be at least 16 years old to register')
+        return bday
 
     class Meta:
         model = User
