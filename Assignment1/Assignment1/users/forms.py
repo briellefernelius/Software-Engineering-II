5ae42db24561550a8bb39d74748cbbb71e449bb3
Vipenjit Singh <vipenjitsingh@mail.weber.edu>
2022-02-03 19:41:07 -0700

    changes

--- Assignment1/Assignment1/users/forms.py
+++ Assignment1/Assignment1/users/forms.py
@@ -6,7 +6,6 @@ from django.forms import ModelForm, Select
 from .models import Course
 from .models import *
 
-
 options = [('student', 'Student'), ('instructor', 'Instructor'), ]
 
 
@@ -16,7 +15,7 @@ class RegistrationForm(UserCreationForm):
 
     def clean_birthday(self):
         bday = self.cleaned_data['birthday']
-        age = (date.today() - bday).days/362
+        age = (date.today() - bday).days / 362
         if age < 16:
             raise forms.ValidationError('Must be at least 16 years old to register')
         return bday
@@ -42,3 +41,7 @@ class CourseForm(ModelForm):
     class Meta:
         model = Course
         fields = ['department', 'course_number', 'course_name', 'credit_hours']
+
+
+class ProfileForm(ModelForm):
+    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
--- Assignment1/Assignment1/users/models.py
+++ Assignment1/Assignment1/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 
 # Create your models here.
@@ -9,6 +10,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField()
     userstatus = models.CharField(max_length=20, default="active")
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email_address = models.EmailField(max_length=50, blank=True, null=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    address_line1 = models.CharField(max_length=1024, blank=True)
+    address_line2 = models.CharField(max_length=1024, blank=True)
+    city = models.CharField(max_length=1024, blank=True)
+
     image_profile = models.ImageField(upload_to='images', default='images/non/noimg.jpg')
 
     def __str__(self):
