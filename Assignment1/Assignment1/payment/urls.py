30819b98a37931e7e9735a147c0f77de8d19d906
London Adams <londonadams@mail.weber.edu>
2022-03-03 10:32:14 -0700

    Payment

--- Assignment1/Assignment1/payment/urls.py
+++ Assignment1/Assignment1/payment/urls.py
@@ -6,4 +6,6 @@ app_name = 'payment'
 
 urlpatterns = [
     path('account/', views.account, name="account"),
+    path('paymentSuccess/', views.PaymentView.as_view(), name="payment_success"),
+    path('paymentFail/', views.PaymentView.as_view(), name="payment_fail"),
 ]
--- Assignment1/Assignment1/payment/views.py
+++ Assignment1/Assignment1/payment/views.py
@@ -1,12 +1,14 @@
 from django.db.models import Sum
 from course.models import Course, CourseUser
 from users.models import CustomUser
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .forms import *
 from .models import *
-from django.views import View
 import stripe
+from django.views.generic import View
 # Create your views here.
+
+stripe.api_key = "sk_test_51KUIyfJVOaFYMiYoXv92CzSa1vIinDxC9OcaZBQY61oQOkxC3ZAuBMSKD58vbsOAMy6ANXI5zVMyL2OSasKjoo8X00SDET2xJG"
 
 
 def account(request):
@@ -22,4 +24,35 @@ def account(request):
     return render(request, 'payment/account.html', {'item_list': item_list, 'form': form, 'tuition': tuition})
 
 
+class PaymentView(View):
+    def post(self, request, *args, **kwargs):
+        print(f"Hello world")
+        card_num = request.POST.get['card_number']
+        exp_month = request.POST.get['expiration_date'.format(date.month)]
+        exp_year = request.POST.get['expiration_date'.format(date.year)]
+        cvc = request.POST.get['cvc_number']
 
+        token = stripe.Token.create(
+          card={
+            "number": card_num,
+            "exp_month": int(exp_month),
+            "exp_year": int(exp_year),
+            "cvc": cvc
+          },
+        )
+
+        charge = stripe.Charge.create(
+          amount=Account.amount * 100,
+          currency="usd",
+          source=token.id,  # obtained above
+          #source="tok_visa", # obtained with Stripe.js (JS)
+          description="Tuition Charge"
+        )
+        #
+        # if charge['captured'] == True:
+        #     Account.objects.create(amount=Account.amount)
+        #     return redirect('payment:success_page')
+        #
+        # return redirect('payment:fail_page')
+
+        return render(request, 'payment/account.html')
